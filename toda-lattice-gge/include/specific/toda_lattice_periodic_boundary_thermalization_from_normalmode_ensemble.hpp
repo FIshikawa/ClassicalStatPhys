03455324ad1046330f8feca4c2df96d6111f18f1
F Ishikawa <fumihiro@exa.phys.s.u-tokyo.ac.jp>
2019-12-21 19:18:10 +0900

    create specific configurations

---
+++ toda-lattice-gge/include/specific/toda_lattice_periodic_boundary_thermalization_from_normalmode_ensemble.hpp
@@ -0,0 +1,68 @@
+#include <boost/lexical_cast.hpp>
+#include <clstatphys/ensemble/modeoccupancy_ensemble_periodic_boundary_fftw.hpp>
+#include <clstatphys/physics/todalattice.hpp>
+#include <common/settings_common.hpp>
+
+using Ensembler = ensemble::ModeOccupancyEnsemblePeriodicBoundaryFFTW; 
+using Hamiltonian = hamiltonian::TodaLattice;
+using Lattice = lattice::Chain;
+
+struct Settings : public SettingsCommon{
+  double J = 1.0; //interaction constant;
+  double alpha = 1.0; //interaction constant;
+
+  Settings(int argc, char **argv, int & input_counter) 
+    : SettingsCommon(argc, argv, input_counter) 
+  { 
+    set(argc, argv, input_counter);
+  }
+
+  inline void set(int argc, char **argv, int & input_counter){
+    if (argc > input_counter) J =     boost::lexical_cast<double>(argv[input_counter]);++input_counter;
+    if (argc > input_counter) alpha = boost::lexical_cast<double>(argv[input_counter]);++input_counter;
+  }
+
+  template <class Dataput>
+  inline void declare(Dataput & dataput){
+    SettingsCommon::declare(dataput);
+
+    std::cout << "I called !!" << std::endl;
+    dataput <<  "System Depend settings " << std::endl
+            <<  Hamiltonian::name() << std::endl
+            <<  Ensembler::name() << std::endl
+            <<  Lattice::name() << std::endl
+            << "Coupling constant : J = " << J << std::endl
+            << "Coupling constant : alpha = " << alpha << std::endl;
+  }
+
+  Hamiltonian hamiltonian(){
+    Lattice lattice_t(Ns);
+
+    std::vector<std::vector<int> > pair_table(
+                                              num_particles,
+                                              std::vector<int>(N_adj)
+                                              ); 
+    lattice_t.create_table(pair_table);
+
+    Hamiltonian hamiltonian_t(
+                              num_particles,
+                              J,
+                              alpha,
+                              pair_table,
+                              N_adj
+                              );
+    return hamiltonian_t;
+  }
+
+  Ensembler ensembler(){
+    Ensembler ensembler_t(
+                          num_particles,
+                          k_initial,
+                          N_normalmode,
+                          E_initial
+                          );
+    return ensembler_t;
+  }
+  
+};
+
