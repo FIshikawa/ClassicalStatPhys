6b08c44a20eea7cd7b9ab5d4ecc6240ac6f828a9
F Ishikawa <fumihiro@exa.phys.s.u-tokyo.ac.jp>
2019-12-22 19:35:07 +0900

    update

--- toda-lattice-gge/include/specific/toda_lattice_periodic_boundary_thermalization_from_normalmode_ensemble.hpp
+++ toda-lattice-gge/include/specific/toda_lattice_periodic_boundary_thermalization_from_normalmode_ensemble.hpp
@@ -1,3 +1,6 @@
+#ifndef SETTINGS_HPP
+#define SETTINGS_HPP
+
 #include <boost/lexical_cast.hpp>
 #include <clstatphys/ensemble/modeoccupancy_ensemble_periodic_boundary_fftw.hpp>
 #include <clstatphys/physics/todalattice.hpp>
@@ -5,7 +8,6 @@
 
 using Ensembler = ensemble::ModeOccupancyEnsemblePeriodicBoundaryFFTW; 
 using Hamiltonian = hamiltonian::TodaLattice;
-using Lattice = lattice::Chain;
 
 struct Settings : public SettingsCommon{
   double J = 1.0; //interaction constant;
@@ -16,6 +18,7 @@ struct Settings : public SettingsCommon{
   { 
     set(argc, argv, input_counter);
   }
+  Settings() = default;
 
   inline void set(int argc, char **argv, int & input_counter){
     if (argc > input_counter) J =     boost::lexical_cast<double>(argv[input_counter]);++input_counter;
@@ -26,13 +29,12 @@ struct Settings : public SettingsCommon{
   inline void declare(Dataput & dataput){
     SettingsCommon::declare(dataput);
 
-    std::cout << "I called !!" << std::endl;
-    dataput <<  "System Depend settings " << std::endl
-            <<  Hamiltonian::name() << std::endl
-            <<  Ensembler::name() << std::endl
-            <<  Lattice::name() << std::endl
-            << "Coupling constant : J = " << J << std::endl
-            << "Coupling constant : alpha = " << alpha << std::endl;
+    dataput <<  "<<System Depend Settings>> " << std::endl
+            <<  "  " << Hamiltonian::name() << std::endl
+            <<  "  " << Ensembler::name() << std::endl
+            <<  "  " << Lattice::name() << std::endl
+            << "  Coupling constant : J = " << J << std::endl
+            << "  Coupling constant : alpha = " << alpha << std::endl;
   }
 
   Hamiltonian hamiltonian(){
@@ -66,3 +68,4 @@ struct Settings : public SettingsCommon{
   
 };
 
+#endif
